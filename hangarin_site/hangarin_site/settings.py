b81618faa128484254d4c13fdf781577226c4f21
Mj Aguilar <marifejoyaguilar3@gmail.com>
2026-03-22 13:27:18 +0800

    Update base.html and Bootstrap layout for Hangarin dashboard

--- hangarin_site/hangarin_site/settings.py
+++ hangarin_site/hangarin_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
     'tasks',
 ]
 
@@ -54,14 +56,15 @@ ROOT_URLCONF = 'hangarin_site.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
@@ -115,4 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
--- hangarin_site/tasks/views.py
+++ hangarin_site/tasks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Task  # Tandaan: Dapat may Task model ka na sa models.py
 
-# Create your views here.
+class HomePageView(ListView):
+    model = Task
+    context_object_name = 'home'
+    template_name = "home.html"
